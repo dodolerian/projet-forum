f81fca59216b9bc355a8692bbd6f47386fd505ec
SulienP <112190974+SulienP@users.noreply.github.com>
2023-05-12 16:46:37 +0200

    ajout de la db

---
+++ main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"forum"
+)
+
+func main() {
+		forum.CreateDatabase()
+
+	WebServer()
+	fmt.Println("hjklfef")
+}
+
+func WebServer() {
+
+	http.HandleFunc("/", Home)
+
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
+
+	fmt.Println("Starting server at port 8871 : http://localhost:3333")
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/Home.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
