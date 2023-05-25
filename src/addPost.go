d0721d5fba75b71a7e588f653b443d3a117a8e71
sulien <sulien.payraudeau@ynov.com>
2023-05-25 13:37:43 +0200

    ajout d'un post dans la bdd

--- src/addPost.go
+++ src/addPost.go
@@ -4,24 +4,25 @@ import (
 	"database/sql"
 	"log"
 	"strconv"
-
+	    "time"
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func AddPost(db *sql.DB , content string, homePage HomePageStruct ){
+	   currentTime := time.Now()
 
-	records := `INSERT INTO POST(author,like,dislike,date ) VALUES (?,?,?,?,?)`
+	records := `INSERT INTO POST(author,content,like,dislike,date ) VALUES (?,?,?,?,?)`
 	query, err := db.Prepare(records)
 	idAuthor := homePage.IdAuthor
 	idAuthorIntoInt ,_:= strconv.Atoi(idAuthor)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = query.Exec(idAuthorIntoInt, content, 0,0)
+	_, err = query.Exec(idAuthorIntoInt, content, 0,0, currentTime)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println( content)
+		fmt.Println("envoie dans la bdd")
 }
