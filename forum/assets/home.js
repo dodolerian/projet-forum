be7982ff02f795984441d4886c9e6f972bcfa6d9
sulien <sulien.payraudeau@ynov.com>
2023-06-19 15:36:50 +0200

    cgm du docker + merge

---
+++ forum/assets/home.js
@@ -0,0 +1,17 @@
+let user = document.getElementById("user");
+
+let popUp = document.getElementById("popUpUser");
+
+user.addEventListener("click", () => {
+  if (popUp.style.display == "flex") {
+    popUp.style.display = "none";
+  } else {
+    popUp.style.display = "flex";
+  }
+});
+
+let exitPost = document.getElementById("exitPost");
+
+exitPost.addEventListener("click", () => {
+  popUp.style.display = "none";
+});
